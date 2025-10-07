e9b298bb3bc16fc219ead1443e507ab8152e21f3
riccardo-guido <guidoriccardo02@gmail.com>
2025-10-07 13:27:14 +0200

    fixed BASE_URL and API

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,24 @@
-const BASE_URL = "http://localhost:3000/smartphones";
+import { BASE_URL } from "../config";
 
-export const fetchAll = async () => {
-  const res = await fetch(BASE_URL);
+const SMARTPHONES_ENDPOINT = `${BASE_URL}/smartphones`;
+
+async function handleResponse(res) {
+  if (!res.ok) {
+    const text = await res.text().catch(() => "");
+    const message = text || `Request failed with status ${res.status}`;
+    const err = new Error(message);
+    err.status = res.status;
+    throw err;
+  }
   return res.json();
+}
+
+export const fetchAllSmartphones = async () => {
+  const res = await fetch(SMARTPHONES_ENDPOINT);
+  return handleResponse(res);
 };
 
-export const fetchById = async (id) => {
-  const res = await fetch(`${BASE_URL}/${id}`);
-  return res.json();
+export const fetchSmartphoneById = async (id) => {
+  const res = await fetch(`${SMARTPHONES_ENDPOINT}/${id}`);
+  return handleResponse(res);
 };
